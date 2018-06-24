569efc987da9815a5dce2a71e4d7d01d1ba15225
Hygor <hygor.christian@gmail.com>
2018-06-24 13:20:26 -0300

    Primeiro commit

---
+++ teste/teste/teste/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  teste
+//
+//  Created by Hygor Dias on 23/06/2018.
+//  Copyright © 2018 Hygor Dias. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
